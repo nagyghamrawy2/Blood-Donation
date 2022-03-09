d789d75a6e240d6ef51caa54247de6c723e15032
projectflutter9 <projectflutter9@gmail.com>
2022-03-10 01:46:35 +0200

    ghamrawy 10/3/2022  تعديل مقاسات

--- lib/Forget Password/Forget_password.dart
+++ lib/Forget Password/Forget_password.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../shared/components/components.dart';
 
 class ForgetPassword extends StatelessWidget {
-  var emailcontroller=TextEditingController();
+  var emailcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +12,19 @@ class ForgetPassword extends StatelessWidget {
         backgroundColor: Color.fromRGBO(237, 57, 74, 1),
         title: Text('Forget Password'),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.notifications))
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications))
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 80,),
+            SizedBox(
+              height: 80,
+            ),
             Center(
               child: Container(
-                width:MediaQuery.of(context).size.width * 0.9,
-                height: MediaQuery.of(context).size.height *0.13,
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: MediaQuery.of(context).size.height * 0.13,
                 decoration: BoxDecoration(
                     boxShadow: [
                       BoxShadow(
@@ -32,31 +34,35 @@ class ForgetPassword extends StatelessWidget {
                         offset: Offset(0, 3), // changes position of shadow
                       ),
                     ],
-                    color:Color.fromRGBO(182, 182, 182, 1),
-                    borderRadius: BorderRadius.only(topRight:Radius.circular(25.0),bottomLeft:Radius.circular(25.0), )
-                ),
+                    color: Color.fromRGBO(182, 182, 182, 1),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(25.0),
+                      bottomLeft: Radius.circular(25.0),
+                    )),
                 child: Column(
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(left: 5.0,top: 8.0),
+                      padding: const EdgeInsets.only(left: 5.0, top: 8.0),
                       child: Align(
-                        alignment:  Alignment.topLeft,
-                        child: Text('Enter the email associated with your account and we`ll send an email with instructions to rest your password.',
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Enter the email associated with your account and we`ll send an email with instructions to rest your password.',
                           style: TextStyle(
                               color: Colors.white,
-                              fontSize: 17,fontWeight: FontWeight.bold
-                          ),),
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-            SizedBox(height: 60,),
+            SizedBox(
+              height: 60,
+            ),
             Textformfield_with_border(
               controller_Name: emailcontroller,
-              num_width: 0.9,
-              num_hieght: 70,
               keyboardtype: TextInputType.emailAddress,
               obsecure: false,
               text_hint: 'Enter your email',
@@ -68,12 +74,14 @@ class ForgetPassword extends StatelessWidget {
               // img_width: 50,
               // img_height: 20
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Buttons_without_icon(
                 num_width: 0.9,
                 num_hieght: 52,
                 text_button_name: 'Send email',
-                button_color:Color.fromRGBO(237, 57, 74, 1),
+                button_color: Color.fromRGBO(237, 57, 74, 1),
                 num_border: 11,
                 num_fontsize: 20,
                 text_fontwwieght: FontWeight.normal)
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Buttons_without_icon extends StatelessWidget {
-  Buttons_without_icon({required this.num_width ,required this.num_hieght ,required this.text_button_name ,required this.button_color ,required this.num_border ,required this.num_fontsize ,required this.text_fontwwieght});
+  Buttons_without_icon(
+      {required this.num_width,
+      required this.num_hieght,
+      required this.text_button_name,
+      required this.button_color,
+      required this.num_border,
+      required this.num_fontsize,
+      required this.text_fontwwieght});
   late double num_width;
   late double num_hieght;
   late String text_button_name;
@@ -10,32 +17,34 @@ class Buttons_without_icon extends StatelessWidget {
   late double num_fontsize;
   late FontWeight text_fontwwieght;
 
-
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       width: MediaQuery.of(context).size.width * num_width,
       height: num_hieght,
       child: ElevatedButton(
-        child:  Text(text_button_name),
+        child: Text(text_button_name),
         onPressed: () {},
         style: ElevatedButton.styleFrom(
             primary: button_color,
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(num_border)
-            ),
-
-            textStyle:
-            TextStyle(fontSize: num_fontsize , fontWeight: text_fontwwieght)),
+                borderRadius: BorderRadius.circular(num_border)),
+            textStyle: TextStyle(
+                fontSize: num_fontsize, fontWeight: text_fontwwieght)),
       ),
     );
   }
 }
 
 class Textformfield_with_border extends StatelessWidget {
-  Textformfield_with_border({required this.controller_Name,required this.num_width ,required this.num_hieght ,required this.keyboardtype ,required this.obsecure ,required this.text_hint ,required this.text_label ,required this.num_border ,});
-  late double num_width;
-  late double num_hieght;
+  Textformfield_with_border({
+    required this.controller_Name,
+    required this.keyboardtype,
+    required this.obsecure,
+    required this.text_hint,
+    required this.text_label,
+    required this.num_border,
+  });
   late TextInputType keyboardtype;
   late bool obsecure;
   late String text_hint;
@@ -50,11 +59,11 @@ class Textformfield_with_border extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      width: MediaQuery.of(context).size.width * num_width,
-      height: num_hieght,
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 66,
       child: TextFormField(
-        controller:controller_Name ,
+        controller: controller_Name,
         keyboardType: keyboardtype,
         obscureText: obsecure,
         decoration: InputDecoration(
@@ -62,12 +71,12 @@ class Textformfield_with_border extends StatelessWidget {
           floatingLabelBehavior: FloatingLabelBehavior.always,
           hintStyle: TextStyle(fontSize: 16),
           labelText: text_label,
-          labelStyle: TextStyle(fontSize: 22 , fontWeight: FontWeight.bold ,color: Colors.black),
+          labelStyle: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
           border: OutlineInputBorder(
-              borderSide:  BorderSide(color: Colors.black26, width: 3.0),
-              borderRadius: BorderRadius.circular(num_border)
-          ),
-          focusedBorder:OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black26, width: 3.0),
+              borderRadius: BorderRadius.circular(num_border)),
+          focusedBorder: OutlineInputBorder(
             borderSide: const BorderSide(color: Colors.black26, width: 3.0),
             borderRadius: BorderRadius.circular(num_border),
           ),
@@ -78,8 +87,6 @@ class Textformfield_with_border extends StatelessWidget {
           //     height: img_height,
           //   ),
           // ),
-
-
         ),
         onChanged: (value) {
           // do something
@@ -90,70 +97,19 @@ class Textformfield_with_border extends StatelessWidget {
 }
 
 class Textformfield_with_icon extends StatelessWidget {
-  Textformfield_with_icon({required this.controller_Name,required this.text_name , required this.num_width ,required this.num_height ,required this.keyboardtype ,required this.obsecure ,required this.text_hint ,required this.img_right_padding ,required this.img_bottom_padding ,required this.icon_name ,required this.img_width ,required this.img_height});
-  late String text_name;
-  late double num_width;
-  late double num_height;
-  late TextInputType keyboardtype;
-  late bool obsecure;
-  late String text_hint;
-  late double img_right_padding;
-  late double img_bottom_padding;
-  late String icon_name;
-  late double img_width;
-  late double img_height;
-  TextEditingController controller_Name;
-  @override
-  Widget build(BuildContext context) {
-    return  Column(
-      children: [
-        Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            child: Text(
-              text_name,
-              style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 18),
-            ),
-          ),
-        ),
-        SizedBox(height: 10,),
-        Align(
-          alignment: Alignment.topLeft,
-          child: Container(
-            width: MediaQuery.of(context).size.width * num_width,
-            height: num_height,
-            child: Material(
-              elevation: 10.0,
-              shadowColor: Colors.white,
-              child: TextField(
-                controller: controller_Name,
-                keyboardType: keyboardtype,
-                obscureText: obsecure,
-                decoration: InputDecoration(
-                  hintText: text_hint,
-                  contentPadding: EdgeInsets.all(15),
-                  hintStyle: TextStyle(fontSize: 16),
-                  border: InputBorder.none,
-                  suffixIcon: Padding(
-                    padding:  EdgeInsets.only(right: img_right_padding ,bottom: img_bottom_padding),
-                    child: Image.asset(icon_name
-                      ,width: img_width,
-                      height: img_height,
-
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class Textformfield2_with_border_with_icon extends StatelessWidget {
-  Textformfield2_with_border_with_icon({required this.controller_Name,required this.text_name , required this.num_width , required this.num_height ,required this.keyboardtype , required this.obsecure , required this.text_hint ,required this.img_right_padding, required this.img_bottom_padding ,required this.icon_name , required this.img_width , required this.img_height});
+  Textformfield_with_icon(
+      {required this.controller_Name,
+      required this.text_name,
+      required this.num_width,
+      required this.num_height,
+      required this.keyboardtype,
+      required this.obsecure,
+      required this.text_hint,
+      required this.img_right_padding,
+      required this.img_bottom_padding,
+      required this.icon_name,
+      required this.img_width,
+      required this.img_height});
   late String text_name;
   late double num_width;
   late double num_height;
@@ -175,11 +131,91 @@ class Textformfield2_with_border_with_icon extends StatelessWidget {
           child: Container(
             child: Text(
               text_name,
-              style: TextStyle(fontWeight: FontWeight.bold ,fontSize: 17),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
           ),
         ),
-        SizedBox(height: 10,),
+        SizedBox(
+          height: 10,
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            width: MediaQuery.of(context).size.width * num_width,
+            height: num_height,
+            child: Material(
+              elevation: 10.0,
+              shadowColor: Colors.white,
+              child: TextField(
+                controller: controller_Name,
+                keyboardType: keyboardtype,
+                obscureText: obsecure,
+                decoration: InputDecoration(
+                  hintText: text_hint,
+                  contentPadding: EdgeInsets.all(15),
+                  hintStyle: TextStyle(fontSize: 16),
+                  border: InputBorder.none,
+                  suffixIcon: Padding(
+                    padding: EdgeInsets.only(
+                        right: img_right_padding, bottom: img_bottom_padding),
+                    child: Image.asset(
+                      icon_name,
+                      width: img_width,
+                      height: img_height,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Textformfield2_with_border_with_icon extends StatelessWidget {
+  Textformfield2_with_border_with_icon(
+      {required this.controller_Name,
+      required this.text_name,
+      required this.num_width,
+      required this.num_height,
+      required this.keyboardtype,
+      required this.obsecure,
+      required this.text_hint,
+      required this.img_right_padding,
+      required this.img_bottom_padding,
+      required this.icon_name,
+      required this.img_width,
+      required this.img_height});
+  late String text_name;
+  late double num_width;
+  late double num_height;
+  late TextInputType keyboardtype;
+  late bool obsecure;
+  late String text_hint;
+  late double img_right_padding;
+  late double img_bottom_padding;
+  late String icon_name;
+  late double img_width;
+  late double img_height;
+  TextEditingController controller_Name;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            child: Text(
+              text_name,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
         Align(
           alignment: Alignment.topLeft,
           child: Container(
@@ -194,19 +230,20 @@ class Textformfield2_with_border_with_icon extends StatelessWidget {
                 contentPadding: EdgeInsets.all(15),
                 hintStyle: TextStyle(fontSize: 16),
                 border: OutlineInputBorder(
-                    borderSide:  BorderSide(color: Colors.black12, width: 2.0),
-                    borderRadius: BorderRadius.circular(10)
-                ),
-                focusedBorder:OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black26, width: 2.0),
+                    borderSide: BorderSide(color: Colors.black12, width: 2.0),
+                    borderRadius: BorderRadius.circular(10)),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                      const BorderSide(color: Colors.black26, width: 2.0),
                   borderRadius: BorderRadius.circular(10),
                 ),
                 suffixIcon: Padding(
-                  padding:  EdgeInsets.only(right: img_right_padding ,bottom: img_bottom_padding),
-                  child: Image.asset(icon_name
-                    ,width: img_width,
+                  padding: EdgeInsets.only(
+                      right: img_right_padding, bottom: img_bottom_padding),
+                  child: Image.asset(
+                    icon_name,
+                    width: img_width,
                     height: img_height,
-
                   ),
                 ),
               ),
@@ -217,8 +254,17 @@ class Textformfield2_with_border_with_icon extends StatelessWidget {
     );
   }
 }
+
 class Container_with_button extends StatelessWidget {
-  Container_with_button({required this.num_width ,required this.num_height ,required this.img_left_padding ,required this.img_bottom_padding ,required this.icon_name ,required this.img_width,required this.img_height ,required this.label_name});
+  Container_with_button(
+      {required this.num_width,
+      required this.num_height,
+      required this.img_left_padding,
+      required this.img_bottom_padding,
+      required this.icon_name,
+      required this.img_width,
+      required this.img_height,
+      required this.label_name});
   late double num_width;
   late double num_height;
   late double img_left_padding;
@@ -230,7 +276,7 @@ class Container_with_button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
+      onTap: () {},
       child: Container(
         width: MediaQuery.of(context).size.width * num_width,
         height: num_height,
@@ -238,19 +284,19 @@ class Container_with_button extends StatelessWidget {
         child: Row(
           children: [
             Padding(
-              padding:  EdgeInsets.only(left: img_left_padding ,bottom: img_bottom_padding),
+              padding: EdgeInsets.only(
+                  left: img_left_padding, bottom: img_bottom_padding),
               child: Image.asset(
                 icon_name,
                 width: img_width,
                 height: img_height,
-
               ),
             ),
-            SizedBox(width: 5,),
-            Text(
-                label_name,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold )
+            SizedBox(
+              width: 5,
             ),
+            Text(label_name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
           ],
         ),
       ),
@@ -283,7 +329,9 @@ class Button_with_icon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {Function;},
+      onTap: () {
+        Function;
+      },
       child: Container(
         decoration: BoxDecoration(
           color: color,
@@ -317,17 +365,17 @@ class Button_with_icon extends StatelessWidget {
 class Requst_textformfield extends StatelessWidget {
   Requst_textformfield(
       {required this.controller_Name,
-        required this.text_name,
-        required this.num_width,
-        required this.num_height,
-        required this.keyboardtype,
-        required this.obsecure,
-        required this.text_hint,
-        required this.img_right_padding,
-        required this.img_bottom_padding,
-        required this.icon_name,
-        required this.img_width,
-        required this.img_height});
+      required this.text_name,
+      required this.num_width,
+      required this.num_height,
+      required this.keyboardtype,
+      required this.obsecure,
+      required this.text_hint,
+      required this.img_right_padding,
+      required this.img_bottom_padding,
+      required this.icon_name,
+      required this.img_width,
+      required this.img_height});
 
   late String text_name;
   late double num_width;
@@ -393,10 +441,20 @@ class Requst_textformfield extends StatelessWidget {
     );
   }
 }
+
 class Textformfield_with_border_with_icon2 extends StatelessWidget {
-  Textformfield_with_border_with_icon2({required this.controller_Name,required this.num_width ,required this.num_hieght ,required this.keyboardtype ,required this.obsecure ,required this.text_hint ,required this.text_label ,required this.num_border ,required this.img_right_padding ,required this.img_bottom_padding ,required this.icon_name ,required this.img_width ,required this.img_height});
-  late double num_width;
-  late double num_hieght;
+  Textformfield_with_border_with_icon2(
+      {required this.controller_Name,
+      required this.keyboardtype,
+      required this.obsecure,
+      required this.text_hint,
+      required this.text_label,
+      required this.num_border,
+      required this.img_right_padding,
+      required this.img_bottom_padding,
+      required this.icon_name,
+      required this.img_width,
+      required this.img_height});
   late TextInputType keyboardtype;
   late bool obsecure;
   late String text_hint;
@@ -411,11 +469,11 @@ class Textformfield_with_border_with_icon2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      width: MediaQuery.of(context).size.width * num_width,
-      height: num_hieght,
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 66,
       child: TextFormField(
-        controller:controller_Name ,
+        controller: controller_Name,
         keyboardType: keyboardtype,
         obscureText: obsecure,
         decoration: InputDecoration(
@@ -423,24 +481,24 @@ class Textformfield_with_border_with_icon2 extends StatelessWidget {
           floatingLabelBehavior: FloatingLabelBehavior.always,
           hintStyle: TextStyle(fontSize: 16),
           labelText: text_label,
-          labelStyle: TextStyle(fontSize: 22 , fontWeight: FontWeight.bold ,color: Colors.black),
+          labelStyle: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
           border: OutlineInputBorder(
-              borderSide:  BorderSide(color: Colors.black26, width: 3.0),
-              borderRadius: BorderRadius.circular(num_border)
-          ),
-          focusedBorder:OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black26, width: 3.0),
+              borderRadius: BorderRadius.circular(num_border)),
+          focusedBorder: OutlineInputBorder(
             borderSide: const BorderSide(color: Colors.black26, width: 3.0),
             borderRadius: BorderRadius.circular(num_border),
           ),
           suffixIcon: Padding(
-            padding:  EdgeInsets.only(right: img_right_padding ,bottom: img_bottom_padding),
-            child: Image.asset(icon_name
-              ,width: img_width,
+            padding: EdgeInsets.only(
+                right: img_right_padding, bottom: img_bottom_padding),
+            child: Image.asset(
+              icon_name,
+              width: img_width,
               height: img_height,
             ),
           ),
-
-
         ),
         onChanged: (value) {
           // do something
@@ -449,7 +507,3 @@ class Textformfield_with_border_with_icon2 extends StatelessWidget {
     );
   }
 }
-
-
-
-
