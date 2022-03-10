5ba4f790e943c5d08f4694daf14bb3621036689c
MohamedTalalsalem <mhamedtalalsalem@gmail.com>
2022-03-10 18:27:40 +0200

    login screen edit

--- lib/Login_Screen/Login_Screen.dart
+++ lib/Login_Screen/Login_Screen.dart
@@ -1,35 +1,33 @@
+import 'package:blood_bank/shared/components/components.dart';
 import 'package:flutter/material.dart';
-import '../shared/components/components.dart';
 
 class LoginScreen extends StatelessWidget {
+
   var emailcontroller = TextEditingController();
   var passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:
-      SingleChildScrollView(
+    return Scaffold
+      (
+      body:  SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              width: double.infinity,
-              height: 220,
-              decoration: BoxDecoration(
+            ClipPath(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height:280,
                 color: Color.fromRGBO(237, 57, 74, 1),
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(130),
-                    bottomLeft: Radius.circular(130),
-                  ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Welcome back,',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
+                    Text('Login !',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
+                  ],
+                ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Welcome back,',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
-                  Text('Login in !',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),),
-                ],
-              ),
+              clipper: CustomClipPath(),
             ),
-            SizedBox(height: 80,),
+            SizedBox(height: 40,),
             Textformfield_with_border_with_icon2(
                 controller_Name: emailcontroller,
                 keyboardtype: TextInputType.emailAddress,
@@ -55,34 +53,34 @@ class LoginScreen extends StatelessWidget {
                 icon_name: 'assets/images/lock.png',
                 img_width: 50,
                 img_height: 20),
-                Row(
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 40.0),
-                        child: Text('Remember me',style: TextStyle(color: Colors.grey)),
-                      ),
-                    ),
-                    MaterialButton(
-                      onPressed: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 5),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                            child: Text('Forget password ?',style: TextStyle(color: Colors.grey),)),
-                      ),
-                    ),
-                  ],
-                )
-                // MaterialButton(
-                //   onPressed: () {},
-                //   child: Padding(
-                //     padding: const EdgeInsets.only(right: 5),
-                //     child: Align(
-                //       alignment: Alignment.centerRight,
-                //         child: Text('Forget password ?')),
-                //   ),
-                // ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 40.0),
+                    child: Text('Remember me',style: TextStyle(color: Colors.grey)),
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 5),
+                    child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('Forget password ?',style: TextStyle(color: Colors.grey),)),
+                  ),
+                ),
+              ],
+            )
+            // MaterialButton(
+            //   onPressed: () {},
+            //   child: Padding(
+            //     padding: const EdgeInsets.only(right: 5),
+            //     child: Align(
+            //       alignment: Alignment.centerRight,
+            //         child: Text('Forget password ?')),
+            //   ),
+            // ),
             ,SizedBox(height: 60,),
             Buttons_without_icon(
                 num_width: 0.45,
@@ -97,4 +95,21 @@ class LoginScreen extends StatelessWidget {
       ),
     );
   }
+}
+class CustomClipPath extends CustomClipper<Path> {
+  var radius=10.0;
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 130);
+    path.quadraticBezierTo(
+        size.width / 2, size.height,
+        size.width, size.height - 130);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
